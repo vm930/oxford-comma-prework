3b58e5fd8068b3b2e7d2ca819792e2f7fd6fc420
abstract-animation-9914 <vm930@hotmail.com>
2019-01-02 15:38:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
   insert_index = array.size
-  array[insert_index] = "and #{array[insert_index]}"
+  array[insert_index -1] = "and #{array[insert_index -1]}"
   array.join(", ")
 end
