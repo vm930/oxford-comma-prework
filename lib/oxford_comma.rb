8fc296dffc15b0e1ce2aec9da751ea1cf44819ad
abstract-animation-9914 <vm930@hotmail.com>
2019-01-02 15:43:21 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,6 @@
 def oxford_comma(array)
   insert_index = array.size
-  if insert_index > 1 
+  if insert_index > 1 and insert_index !===2
   array[insert_index -1] = "and #{array[insert_index -1]}"
   array.join(", ")
 elsif insert_index === 2
